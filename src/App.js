80bffebb6e8b258ed9f6e3f93c7e1220227360cf
Anthony Kelly <anthony@dialsource.com>
2020-06-28 21:18:30 -0700

    Signing off for the night

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import BurgerIngredient from './components/Burger/BurgerIngredient/BurgerIngredient';
 
 class App extends Component {
   render(){
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Aux from '../../hoc/Aux';
-import classes from '../Layout/Layout.css';
+import classes from '../Layout/Layout.module.css';
 
 
 const layout = ( props ) => {
